11ed47bd88b8fc44d733e1806845bbb7c1749aa5
armanish-shu <1539122065@qq.com>
2021-05-05 15:52:53 +0800

    bug01

--- mybatis-examples/src/main/java/com/example/mybatisexamples/entity/user01.java
+++ mybatis-examples/src/main/java/com/example/mybatisexamples/entity/user01.java
@@ -3,6 +3,7 @@ package com.example.mybatisexamples.entity;
 public class user01 {
     public void add(){
         int x=10;
-        int y=10/0;
+        int y=10/x;
     }
+    public void fixbug(){}
 }
